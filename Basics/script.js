0e5e2ca2859b59bdaa6c5c59d7d54a9ffca3ba51
Ashwini <ashwiniaski23@gmail.com>
2025-11-11 12:29:38 -0500

    Basics of programming

---
+++ Basics/script.js
@@ -0,0 +1,44 @@
+"use strict";
+// Programming 101
+console.log("Hello World!");
+console.log("Ashwini Patil");
+
+const a = 10;
+const b = 20;
+
+const sum = a + b;
+const multiply = a * b;
+
+console.log(multiply);
+console.log(sum);
+
+let x = 10;
+x = 20;
+console.log(x);
+
+let firstName = "Ashwini";
+let lastName = "Patil";
+
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+let arr = [1, 2, 3, 4];
+console.log(arr);
+console.log(arr[3]);
+
+// Loops 01
+for (let i = 0; i <= 4; i++) {
+  console.log("Hello World!");
+}
+
+for (let i = 3; i < 5; i++) {
+  console.log("Hello World!");
+}
+
+for (let i = 2; i < 9; i = i + 2) {
+  console.log("Hello World!", i);
+}
+
+for (let i = 5; i > 0; i--) {
+  console.log("Hello", i);
+}
